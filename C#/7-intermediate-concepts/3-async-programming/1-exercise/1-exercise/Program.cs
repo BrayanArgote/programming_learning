4f2c481a534a4011f4868657b7dedcd94a47963c
Brayan Sebastian Argote Rojas <jf.zea@dhl.com>
2026-04-01 11:29:59 -0500

    Some async programming exercises.

---
+++ C#/7-intermediate-concepts/3-async-programming/1-exercise/1-exercise/Program.cs
@@ -0,0 +1,53 @@
+﻿
+List<List<string>> database = new List<List<string>> {      
+    new List<string> { "yellow", "green", "red", "white" }, // colors
+    new List<string> { "dog", "cat", "fish" }               // animals
+};
+
+async Task<string> PrintItemsAsync(int listIndex)
+{
+    Console.WriteLine("\n-- Searching... ");
+
+    var data = await SearchItemsAsync(listIndex);
+    return string.Join(" - ", data);
+}
+
+async Task<List<string>> SearchItemsAsync(int listIndex)
+{
+    await Task.Delay(2500);
+    return database[listIndex];
+}
+
+string option;
+
+do
+{
+    Console.WriteLine("\n==== MENU ===" +
+                      "\n1. Show colors. " +
+                      "\n2. Show animals. " +
+                      "\n3. Exit. ");
+    Console.Write("Enter an option: ");
+    option = Console.ReadLine().Trim();
+    
+
+    switch (option)
+    {
+        case "1":
+            Console.WriteLine(await PrintItemsAsync(Convert.ToInt32(option) - 1));
+            break;
+
+        case "2": 
+            Console.WriteLine(await PrintItemsAsync(Convert.ToInt32(option) - 1));
+            break;
+
+        case "3": Console.WriteLine("Exiting... "); break;
+
+        case "cls" or "clear": Console.Clear(); break;
+
+        default: Console.WriteLine("*** Please type a valid option ***"); break;
+    }
+
+} while (option != "3");
+
+Console.ReadKey();
+
---
+++ C#/7-intermediate-concepts/3-async-programming/2-exercise/2-exercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+async Task DownloadBook(string title, int milliseconds)
+{
+    Console.WriteLine("\n-- Starting download --\n");
+    await Task.Delay(milliseconds);
+
+    ProcessStartInfo psi = new ProcessStartInfo
+    {
+        FileName = "cmd.exe",
+        Arguments = $"/c echo ¡-- {title} downloaded successful in {milliseconds / 1000} seconds --! & pause",
+        UseShellExecute = true,
+        CreateNoWindow = false
+    };
+
+    Process.Start(psi);
+}
+
+Console.WriteLine("===== EBOOK DOWNLOADER =====");
+
+int numberBooksDownloaded = 0;
+Random numberRandom = new Random();
+while (true)
+{
+    Console.Write("Enter the name of the book: ");
+    string input = Console.ReadLine().Trim();
+
+    if (string.IsNullOrEmpty(input)) { continue; }
+    if (string.Equals(input, "exit", StringComparison.OrdinalIgnoreCase)) { break; }
+
+    int milliseconds = numberRandom.Next(2000, 9000);
+
+    DownloadBook(input, milliseconds);
+
+    numberBooksDownloaded++;
+}
+
+Console.WriteLine($"{numberBooksDownloaded} books was downloaded --");
+Console.WriteLine("Exiting... ");
+
+Console.ReadKey();
